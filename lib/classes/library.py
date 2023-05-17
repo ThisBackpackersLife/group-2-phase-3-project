c5a4dd92557b40b7c72799d95e3eed645b9d7548
Nicholas <crazedglitch@yahoo.com>
2023-05-17 10:35:13 -0500

    Library & Visitor table creation complete

--- lib/classes/library.py
+++ lib/classes/library.py
@@ -1,4 +1,91 @@
-from . import CONN, CURSOR
+from .__init__ import CONN, CURSOR
 
 class Library:
-    pass
+    
+    def __init__( self, name, city, id=None ):
+        self.name = name
+        self.city = city
+        self.id = id
+
+    @property
+    def name( self ):
+        return self._name
+    
+    @name.setter 
+    def name( self, name ):
+        if isinstance( name, str ) and len( name ) > 0:
+            self._name = name
+        else:
+            raise Exception( "Name must be a string > 0." )
+        
+    @property
+    def city( self ):
+        return self._city
+
+    @city.setter
+    def city( self, city ):
+        if isinstance( city, str ) and len( city ) > 0:
+            self._city = city
+        else:
+            raise Exception( "City must be a string > 0." )
+
+    @classmethod
+    def create_table( cls ):
+        CURSOR.execute("""
+            CREATE TABLE IF NOT EXISTS libraries(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                city TEXT
+            )
+        """
+        )
+        print( "Check console to verify creation was successful." )
+
+    @classmethod
+    def create( cls, name, city ):
+        library = Library( name, city )
+        CURSOR.execute(f"""
+            INSERT INTO libraries( name, city )
+            VALUES( '{ library.name }', '{ library.city }')
+        """
+        )
+        new_library_id = CURSOR.execute( "SELECT last_insert_rowid() FROM libraries" ).fetchone()[0]
+        return cls.find_by_id( new_library_id )
+
+    @classmethod
+    def find_by_id( cls, id ):
+        if isinstance( id, int ) and id > 0:
+            sql = f"SELECT * FROM libraries WHERE id = { id }"
+            new_library = CURSOR.execute( sql ).fetchone()
+            if new_library:
+                return cls.db_into_instance( new_library )
+            else:
+                raise Exception( "Could not find Library with that ID.")
+        else: 
+            raise Exception( "ID must be an number > 0." )
+        
+    @classmethod 
+    def find_by_name( cls, name ):
+        if isinstance( name, str ) and len( name ) > 0:
+            sql = f"SELECT * FROM libraries WHERE name LIKE '{ name }'"
+            libraries = CURSOR.execute( sql ).fetchall()
+            if libraries:
+                return [ cls.db_into_instance( library ) for library in libraries ]
+            else:
+                raise Exception( "Could not find any libraries with that name." )
+        else:
+            raise Exception( "Name must be a string > 0 characters." )
+
+
+    @classmethod
+    def all ( cls ):
+        sql = "SELECT * FROM libraries"
+
+        libraries = CURSOR.execute( sql ).fetchall()
+        return [ cls.db_into_instance( library ) for library in libraries ]
+
+    @classmethod
+    def db_into_instance( cls, library ):
+        return Library( library[1], library[2], library[0] )
+    
+    
--- lib/debug.py
+++ lib/debug.py
@@ -9,9 +9,12 @@ faker = Faker()
 import ipdb
 
 Visitors.create_table()
+Library.create_table()
 
 for n in range( 0, 5 ):
     Visitors.create( faker.first_name(), faker.last_name(), faker.address() )
 
+for n in range( 0, 5 ):
+    Library.create( f"Library { faker.city() }", faker.city()  )
 
 ipdb.set_trace()
